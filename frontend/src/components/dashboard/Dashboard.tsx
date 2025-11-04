43b7e994842003aa2ea253e23d0574f5e2478f5f
Dylan2108 <dylancabrera024@gmail.com>
2025-11-04 00:05:44 -0500

    Add Navbar component

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Sidebar from '../sidebar/Sidebar';
-// import NavBar from '../navbar/Navbar'
-// import "../../styles/dashboard.css"
+import NavBar from '../navbar/Navbar'
+import "../../styles/dashboard.css"
 
 
 const Dashboard : React.FC = () =>{
   return (
     <div className='dashboard-sidebar'>
       <Sidebar/>
-      {/* <div className='dashboard-navbar'>
+      <div className='dashboard-navbar'>
         <NavBar/>
         home Container
-      </div> */}
+      </div>
     </div>
   )
 }
