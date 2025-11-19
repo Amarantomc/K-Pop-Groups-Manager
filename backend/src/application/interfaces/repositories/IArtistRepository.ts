b745dba880b61bca999c75dc68608d40565542dc
Amaranto <danielmares2003@gmail.com>
2025-11-19 09:43:01 -0500

    Adding values objects

--- backend/src/application/interfaces/repositories/IArtistRepository.ts
+++ backend/src/application/interfaces/repositories/IArtistRepository.ts
@@ -1,18 +1,31 @@
 import type { Apprentice } from "../../../domain";
 import type { Artist } from "../../../domain/entities/Artist";
+import type { GroupMembership } from "../../../domain/value objects/GroupMembership";
 import type { CreateArtistDto } from "../../dtos/artist/CreateArtistDto";
 import type { IBaseRepository } from "./IBaseRepository";
 
 export interface IArtistRepository extends IBaseRepository<Artist,CreateArtistDto,any>{
 
         getAll():Promise<Artist[]>
-        getArtistGroup(id:any):Promise<any>
-        getGroupHistory(id:any): Promise<any>
+        findByName(name: string): Promise<Artist[]>;
+        findByStatus(status: string): Promise<Artist[]>;
+        
+        getArtistGroup(id:any):Promise<GroupMembership|null>
+        getGroupHistory(id:any): Promise<GroupMembership[]>
+        
         getRelatedApprentice():Promise<Apprentice>
-        getContracts():Promise<any>
-        getArtistActivites():Promise<any>
-        getArtistReleases():Promise<any>
-        getArtistSol():Promise<any>
+         
+        findActiveContract(artistId: number): Promise<any>;
+        findContractHistory(artistId: number): Promise<any[]>;
+ 
+        findActivities(artistId: number): Promise<any[]>;
+        findAcceptedActivities(artistId: number): Promise<any[]>;
+  
+   
+        findReleases(artistId: number): Promise<any[]>;
+  
+  
+        findGroupRequests(artistId: number): Promise<any[]>;
         
 
 }
