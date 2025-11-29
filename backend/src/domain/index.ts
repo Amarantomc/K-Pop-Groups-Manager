cc5f801ff8719f692ed8c10239625bce64256ca7
Agustin A. Carbajal Romero <agustin030902@gmail.com>
2025-11-28 22:36:56 -0500

    domain implementation visual concept and update index

--- backend/src/domain/index.ts
+++ backend/src/domain/index.ts
@@ -1,5 +1,14 @@
 import User from "./entities/user"
 import Apprentice from "./entities/Apprentice"
 import Song from './entities/Song';
+import Income from './entities/Income';
+import { Activity } from './entities/Activity';
+import Album from './entities/Album';
+import PopularityList from './entities/PopularityList';
+import Award from './entities/Award';
+import Concept from './entities/Concept';
+import Contract from './entities/Contract';
+import Application from './entities/Application';
+import VisualConcept from './entities/VisualConcept';
 
-export {User,Apprentice,Song}
+export {User,Apprentice,Song,Income,Activity,Album,PopularityList,Award,Concept,Contract,Application,VisualConcept}
