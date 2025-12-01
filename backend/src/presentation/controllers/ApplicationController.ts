7719d246b4a4ca31232c629535c529fe16d1bc63
Agustin A. Carbajal Romero <agustin030902@gmail.com>
2025-12-01 00:10:48 -0500

    feat(presentation) add controller application(solicitud)

---
+++ backend/src/presentation/controllers/ApplicationController.ts
@@ -0,0 +1,129 @@
+import { inject, injectable } from "inversify";
+import { Types } from "../../infrastructure/di/Types";
+import type { Request,Response } from "express";
+import type { CreateApplicationUseCase } from "../../application/usesCase/application(solicitud)/CreateApplicatonUseCase";
+import type { GetApplicationUseCase } from "../../application/usesCase/application(solicitud)/GetApplicationUseCase";
+import type { DeleteApplicationUseCase } from "../../application/usesCase/application(solicitud)/DeleteApplicationUseCase";
+import type { ListApplicationUseCase } from "../../application/usesCase/application(solicitud)/ListApplicationUseCase";
+import { CreateApplicationDto } from "../../application/dtos/application(solicitud)/CreateApplicationDto";
+import type { UpdateApplicationUseCase } from "../../application/usesCase/application(solicitud)/UpdateApplicationUseCase";
+import type { FindByApprenticeUseCase } from "../../application/usesCase/application(solicitud)/FindByApprenticeUseCase";
+
+
+@injectable()
+export class ApplicationController{
+    constructor(@inject(Types.CreateApplicationUseCase)  private createApplicationUseCase: CreateApplicationUseCase ,
+                @inject(Types.GetApplicationUseCase) private getApplicationUseCase:GetApplicationUseCase,
+                @inject(Types.DeleteApplicationUseCase) private deleteApplicationUseCase:DeleteApplicationUseCase,
+                @inject(Types.UpdateApplicationUseCase) private updateApplicationUseCase:UpdateApplicationUseCase,
+                @inject(Types.ListApplicationUseCase) private listApplicationUseCase: ListApplicationUseCase,
+                @inject(Types.FindByApprenticeUseCase) private FindByApprenticeUseCase: FindByApprenticeUseCase){}
+
+async createApplication(req: Request, res: Response) 
+      {
+    try {
+      
+      const {apprenticesId} =req.params
+      
+      const applicationDto = CreateApplicationDto.create(req.body, Array.isArray(apprenticesId) ? apprenticesId.map(Number) : null);
+      const application = await this.createApplicationUseCase.execute(applicationDto);
+
+      
+
+      res.status(201).json({
+        success: true,
+        data: application
+      });
+
+   }
+   catch (error: any) {
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
+
+
+}
+
+async getApplication(req: Request, res: Response) 
+{
+  try {
+        const { id } = req.params;
+        const application = await this.getApplicationUseCase.excute(id!);
+
+        res.json({
+          success: true,
+          data: application
+    })
+    
+}
+catch (error: any) {
+    res.status(404).json({
+      success: false,
+      error: error.message
+    });
+  }
+
+
+}
+
+async updateApplication(req: Request, res: Response) 
+{
+  try {
+        const { id } = req.params;
+        const application = await this.updateApplicationUseCase.execute(id!,req.body)
+         
+        res.json({
+          success: true,
+          data: application
+    });
+
+} catch (error: any) {
+res.status(404).json({
+  success: false,
+  error: error.message
+});
+}
+}
+
+async deleteApplication(req: Request, res: Response) {
+    try {
+      
+        const { id } = req.params;
+        const application = await this.deleteApplicationUseCase.execute(id!)
+      
+
+      res.json({
+        success: true,
+        data: application
+      });
+
+    } catch (error: any) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+}
+
+  async listApplication(req: Request, res: Response) {
+    try {
+      
+      const application = await this.listApplicationUseCase.execute()
+      
+
+      res.json({
+        success: true,
+        data: application
+      });
+
+    } catch (error: any) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+}
+
+}
