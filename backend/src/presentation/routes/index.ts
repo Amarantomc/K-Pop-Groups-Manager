63f846834946b7328c86495a9f6f00e6756c62a6
Agustin A. Carbajal Romero <agustin030902@gmail.com>
2025-12-04 13:10:44 -0500

    fix error

--- backend/src/presentation/routes/index.ts
+++ backend/src/presentation/routes/index.ts
@@ -8,11 +8,9 @@ import { GroupRoutes } from "./groupRoutes";
 import { ArtistRoutes } from "./ArtistRoutes";
 import { ConceptRoutes } from "./ConceptRoutes";
 import { ActivityRoutes } from "./ActivityRoutes";
-<<<<<<< HEAD
 import { AwardRoutes } from "./AwardRoutes";
-=======
 import { PopularityListRoutes } from "./PopularityListRoutes";
->>>>>>> origin/master
+
 
 const rootRouter: Router = Router();
 const userRoutes = new UserRoutes();
@@ -24,11 +22,8 @@ const apprenticeRoute=new ApprenticeRoutes()
 const artistRoutes=new ArtistRoutes()
 const conceptRoutes=new ConceptRoutes()
 const activityRoutes = new ActivityRoutes();
-<<<<<<< HEAD
 const awardRoutes = new AwardRoutes();
-=======
 const popularityListRoutes=new PopularityListRoutes()
->>>>>>> origin/master
 
 rootRouter.use("/auth", authRoutes.getRouter());
 rootRouter.use("/user", userRoutes.getRouter());
@@ -40,11 +35,7 @@ rootRouter.use('/apprentice',apprenticeRoute.getRouter())
 rootRouter.use("/artist", artistRoutes.getRouter());
 rootRouter.use("/concept", conceptRoutes.getRouter());
 rootRouter.use("/activity", activityRoutes.getRouter());
-<<<<<<< HEAD
 rootRouter.use("/award", awardRoutes.getRouter());
-
-=======
 rootRouter.use('/populist',popularityListRoutes.getRouter())
->>>>>>> origin/master
 
 export default rootRouter;
