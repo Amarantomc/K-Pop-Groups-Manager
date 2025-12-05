8240ab5c8ee1061fd234f21aaae95b6e6c17d308
Amaranto <danielmares2003@gmail.com>
2025-12-05 07:32:10 -0500

    Domain Implementation

--- backend/src/domain/entities/Contract.ts
+++ backend/src/domain/entities/Contract.ts
@@ -1,30 +1,41 @@
+
 import type { ContractStatus } from "../enums/ContractStatus"
+import type { ContractType } from "../enums/ContractType"
+import type { Agency } from "./Agency"
+import type { Artist } from "./Artist"
+import type { Group } from "./Group"
 
 export default class Contract{
-    readonly idAgency:number
-    readonly apprenticeId:number
-    readonly groupId:number
+    readonly agency:Agency
+    readonly artist?:Artist | undefined
+    readonly group?:Group | undefined
+    readonly idGroupContract?:number | undefined
     readonly startDate:Date|string
-    readonly completionDate ?:Date|string
+    readonly completionDate ?:Date|string |undefined
     readonly incomeDistribution: string
     readonly status: ContractStatus | string
     readonly initialConditions:string
+    readonly type:ContractType
 
     constructor(attrs:{
-        idAgency: number;
-        apprenticeId:number,
-        groupId:number,
+        agency: Agency;
+        artist?: Artist | undefined;
+        group?: Group | undefined;
+        idGroupContract?: number | undefined;
+        type: ContractType;
         startDate: Date|string;
-        completationDate: Date|string;
+        completationDate: Date|string| undefined;
         incomeDistribution: string;
         status: string|ContractStatus;
         initialCondition:string
     })
     {
-        this.idAgency = attrs.idAgency
+        this.agency = attrs.agency
         this.completionDate = attrs.completationDate
-        this.apprenticeId = attrs.apprenticeId
-        this.groupId = attrs.groupId
+        this.artist = attrs.artist
+        this.group = attrs.group
+        this.idGroupContract = attrs.idGroupContract
+        this.type = attrs.type
         this.incomeDistribution = attrs.incomeDistribution
         this.initialConditions = attrs.initialCondition
         this.startDate = attrs.startDate
