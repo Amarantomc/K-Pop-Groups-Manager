782521107e6ac32c712144acac6f8759268ea9e8
Ronald_47 <132956449+Ronald1301@users.noreply.github.com>
2025-12-08 00:42:10 -0500

    Update ActivityRepository.ts

--- backend/src/infrastructure/repositories/ActivityRepository.ts
+++ backend/src/infrastructure/repositories/ActivityRepository.ts
@@ -13,37 +13,37 @@ import type { ArtistOnActivityDto } from "../../application/dtos/activity/Artist
 @injectable()
 export class ActivityRepository implements IActivityRepository {
 
-  constructor( @inject(Types.PrismaClient) private prisma:any,
-                @inject(Types.IUnitOfWork) private unitOfWork:IUnitOfWork) {}
- 
+  constructor(@inject(Types.PrismaClient) private prisma: any,
+    @inject(Types.IUnitOfWork) private unitOfWork: IUnitOfWork) { }
 
 
-   
 
-     private get db() {
+
+
+  private get db() {
     return this.unitOfWork.getTransaction();
-    }
+  }
 
-   
-    async create(data: CreateActivityDto): Promise<Activity> {
-        const activity = await this.db.actividad.create({
+
+  async create(data: CreateActivityDto): Promise<Activity> {
+    const activity = await this.db.actividad.create({
       data: {
         responsable: data.responsible,
         tipoActividad: data.activityType,
         fecha: new Date(data.date),
         lugar: data.place,
         tipoEvento: data.eventType
-      } 
+      }
     });
-    
+
     return ActivityResponseDto.toEntity(activity);
-    }
-    async findById(id: string): Promise<Activity | null> {
-         const activity = await this.db.actividad.findUnique({
+  }
+  async findById(id: string): Promise<Activity | null> {
+    const activity = await this.db.actividad.findUnique({
       where: { id: Number(id) },
       include: {
         Ingreso: true,
-        Artistas: {
+        Personas: {
           include: {
             artista: true
           }
@@ -51,29 +51,29 @@ export class ActivityRepository implements IActivityRepository {
       }
     });
     return activity ? ActivityResponseDto.toEntity(activity) : null;
-    }
-async update(id: string, data: Partial<UpdateActivityDto>): Promise<Activity> {
-     
+  }
+  async update(id: string, data: Partial<UpdateActivityDto>): Promise<Activity> {
+
     const activity = await this.db.actividad.update({
       where: { id: Number(id) },
-      data:{
-         responsable:data.responsible,
-         lugar:data.place,
-         fecha:data.date,
-         tipoActividad:data.activityType
+      data: {
+        responsable: data.responsible,
+        lugar: data.place,
+        fecha: data.date,
+        tipoActividad: data.activityType
       },
       include: {
         Ingreso: true,
         Artistas: true
       }
     });
-    
+
 
     return ActivityResponseDto.toEntity(activity);
-        
-    }
-async delete(id: string): Promise<void> {
-         const activityId = Number(id);
+
+  }
+  async delete(id: string): Promise<void> {
+    const activityId = Number(id);
 
     // Eliminar relaciones primero
     await this.db.ingreso.deleteMany({
@@ -89,16 +89,16 @@ async delete(id: string): Promise<void> {
     });
 
     // Eliminar la actividad
-    await this.db.actividad.delete({
+    await this.db.Actividad.delete({
       where: { id: activityId }
     });
-    }
+  }
 
-async getAll(): Promise<Activity[]> {
+  async getAll(): Promise<Activity[]> {
     const activities = await this.db.actividad.findMany({
       include: {
         Ingreso: true,
-        Artistas: {
+        Personas: {
           include: {
             artista: true
           }
@@ -106,45 +106,45 @@ async getAll(): Promise<Activity[]> {
       }
     });
     return ActivityResponseDto.toEntities(activities);
-    }
-
-    async findByArtist(apprenticeId: number, groupId: number): Promise<Activity[]> {
-       const activities= await this.db.actividad.findMany({
-        where:{
-           Artistas:{
-             some:{
-              idAp:apprenticeId,
-              idGr:groupId
-             }
-           }
-        }
-
-       })
-       return ActivityResponseDto.toEntities(activities)
   }
 
-   async addArtist(command: ArtistOnActivityDto): Promise<void> {
-        await this.db.artistaEnActividad.create({
-          data:{
-            idAct:command.activityId,
-            idAp:command.apprenticeId,
-            idGr:command.groupId,
-            aceptado:command.accepted
+  async findByArtist(apprenticeId: number, groupId: number): Promise<Activity[]> {
+    const activities = await this.db.actividad.findMany({
+      where: {
+        Artistas: {
+          some: {
+            idAp: apprenticeId,
+            idGr: groupId
           }
-        })
+        }
+      }
+
+    })
+    return ActivityResponseDto.toEntities(activities)
   }
 
-   async findByGroup(groupId: number): Promise<Activity[]|null> {
-       const activities=await this.db.PersonasEnActividad.findMany({
-         where:{
-          idGrupos:groupId
-         },
-         include:{
-          actividad:true
-         }
-       })
-       
-       
-       return activities ? ActivityResponseDto.toEntities(activities.map((a:any)=>a.actividad)):null
+  async addArtist(command: ArtistOnActivityDto): Promise<void> {
+    await this.db.artistaEnActividad.create({
+      data: {
+        idAct: command.activityId,
+        idAp: command.apprenticeId,
+        idGr: command.groupId,
+        aceptado: command.accepted
+      }
+    })
+  }
+
+  async findByGroup(groupId: number): Promise<Activity[] | null> {
+    const activities = await this.db.PersonasEnActividad.findMany({
+      where: {
+        idGrupos: groupId
+      },
+      include: {
+        actividad: true
+      }
+    })
+
+
+    return activities ? ActivityResponseDto.toEntities(activities.map((a: any) => a.actividad)) : null
   }
 }
