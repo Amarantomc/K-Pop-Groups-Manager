1b6b905e82827c1898ee5c790c6921533d61b480
Agustin A. Carbajal Romero <agustin030902@gmail.com>
2025-12-11 01:09:01 -0500

    visual concept image configuration

--- backend/src/presentation/controllers/VisualConceptController.ts
+++ backend/src/presentation/controllers/VisualConceptController.ts
@@ -1,7 +1,135 @@
+// import { inject, injectable } from "inversify";
+// import { Types } from "../../infrastructure/di/Types";
+// import type { Request,Response } from "express";
+// import VisualConcept from '../../domain/entities/VisualConcept';
+// import type { CreateVisualConceptUseCase } from "../../application/usesCase/visualConcept/CreateVisualConceptUseCase";
+// import type { GetVisualConceptUseCase } from "../../application/usesCase/visualConcept/GetConceptUseCase";
+// import type { DeleteVisualConceptUseCase } from "../../application/usesCase/visualConcept/DeleteVisualConceptUseCase";
+// import type { UpdateVisualConceptUseCase } from "../../application/usesCase/visualConcept/UpdateVisualConceptUseCase";
+// import type { ListVisualConceptUseCase } from "../../application/usesCase/visualConcept/ListVisualConceptUseCase";
+// import { CreateVisualConceptDto } from "../../application/dtos/visualConcept/CreateVisualConceptDto";
+
+
+// @injectable()
+// export class VisualConceptController {
+    
+// constructor(@inject(Types.CreateVisualConceptUseCase)  private createVisualConceptUseCase: CreateVisualConceptUseCase ,
+//             @inject(Types.GetVisualConceptUseCase) private getVisualConceptUseCase:GetVisualConceptUseCase,
+//             @inject(Types.DeleteVisualConceptUseCase) private deleteVisualConceptUseCase:DeleteVisualConceptUseCase,
+//             @inject(Types.UpdateVisualConceptUseCase) private updateVisualConceptUseCase:UpdateVisualConceptUseCase,
+//             @inject(Types.ListVisualConceptUseCase) private listVisualConceptUseCase:ListVisualConceptUseCase){}
+
+    
+//   async createVisualConcept(req: Request, res: Response) 
+//       {
+//     try {
+        
+//       const image  = req.file;
+//       const data = req.body
+
+//       const VisualConceptDto=CreateVisualConceptDto.create(data)
+//       const VisualConcept = await this.createVisualConceptUseCase.execute(VisualConceptDto);
+
+      
+
+//       res.status(201).json({
+//         success: true,
+//         data: VisualConcept
+//       });
+
+//     } catch (error: any) {
+//       res.status(400).json({
+//         success: false,
+//         error: error.message
+//       });
+//     }
+//       }
+
+//     async getVisualConcept(req: Request, res: Response) 
+//       {
+//         try {
+//               const { id } = req.params;
+//               const VisualConcept = await this.getVisualConceptUseCase.excute(id!);
+               
+               
+
+//               res.json({
+//                 success: true,
+//                 data: VisualConcept
+//           });
+
+//     } catch (error: any) {
+//       res.status(404).json({
+//         success: false,
+//         error: error.message
+//       });
+//     }
+//   }
+
+//     async deleteVisualConcept(req: Request, res: Response) {
+//     try {
+      
+//         const { id } = req.params;
+//         const VisualConcept = await this.deleteVisualConceptUseCase.execute(id!)
+      
+
+//       res.json({
+//         success: true,
+//         data: VisualConcept
+//       });
+
+//     } catch (error: any) {
+//       res.status(500).json({
+//         success: false,
+//         error: error.message
+//       });
+//     }
+//   }
+
+//        async updateVisualConcept(req: Request, res: Response) 
+//       {
+//         try {
+//               const { id } = req.params;
+//               const VisualConcept = await this.updateVisualConceptUseCase.execute(id!,req.body)
+               
+               
+
+//               res.json({
+//                 success: true,
+//                 data: VisualConcept
+//           });
+
+//     } catch (error: any) {
+//       res.status(404).json({
+//         success: false,
+//         error: error.message
+//       });
+//     }
+//   }
+
+//       async listVisualConcept(req: Request, res: Response) {
+//     try {
+      
+//       const VisualConcept = await this.listVisualConceptUseCase.execute()
+      
+
+//       res.json({
+//         success: true,
+//         data: VisualConcept
+//       });
+
+//     } catch (error: any) {
+//       res.status(500).json({
+//         success: false,
+//         error: error.message
+//       });
+//     }
+//   }
+// }
+
 import { inject, injectable } from "inversify";
 import { Types } from "../../infrastructure/di/Types";
-import type { Request,Response } from "express";
-import VisualConcept from '../../domain/entities/VisualConcept';
+import type { Request, Response } from "express";
 import type { CreateVisualConceptUseCase } from "../../application/usesCase/visualConcept/CreateVisualConceptUseCase";
 import type { GetVisualConceptUseCase } from "../../application/usesCase/visualConcept/GetConceptUseCase";
 import type { DeleteVisualConceptUseCase } from "../../application/usesCase/visualConcept/DeleteVisualConceptUseCase";
@@ -10,51 +138,49 @@ import type { ListVisualConceptUseCase } from "../../application/usesCase/visual
 import { CreateVisualConceptDto } from "../../application/dtos/visualConcept/CreateVisualConceptDto";
 
 
+declare global {
+  namespace Express {
+    interface Request {
+      file?: Express.Multer.File;
+    }
+  }
+}
 @injectable()
 export class VisualConceptController {
-    
-constructor(@inject(Types.CreateVisualConceptUseCase)  private createVisualConceptUseCase: CreateVisualConceptUseCase ,
-            @inject(Types.GetVisualConceptUseCase) private getVisualConceptUseCase:GetVisualConceptUseCase,
-            @inject(Types.DeleteVisualConceptUseCase) private deleteVisualConceptUseCase:DeleteVisualConceptUseCase,
-            @inject(Types.UpdateVisualConceptUseCase) private updateVisualConceptUseCase:UpdateVisualConceptUseCase,
-            @inject(Types.ListVisualConceptUseCase) private listVisualConceptUseCase:ListVisualConceptUseCase){}
+  constructor(
+    @inject(Types.CreateVisualConceptUseCase) private createVisualConceptUseCase: CreateVisualConceptUseCase,
+    @inject(Types.GetVisualConceptUseCase) private getVisualConceptUseCase: GetVisualConceptUseCase,
+    @inject(Types.DeleteVisualConceptUseCase) private deleteVisualConceptUseCase: DeleteVisualConceptUseCase,
+    @inject(Types.UpdateVisualConceptUseCase) private updateVisualConceptUseCase: UpdateVisualConceptUseCase,
+    @inject(Types.ListVisualConceptUseCase) private listVisualConceptUseCase: ListVisualConceptUseCase
+  ) {}
 
-    
-  async createVisualConcept(req: Request, res: Response) 
-      {
+  async createVisualConcept(req: Request, res: Response) {
     try {
-          
-      const VisualConceptDto=CreateVisualConceptDto.create(req.body)
+      const imagePath = req.file ? req.file.filename : null;
+      const dtoData = { ...req.body, picture: imagePath };
+      const VisualConceptDto = CreateVisualConceptDto.create(dtoData);
       const VisualConcept = await this.createVisualConceptUseCase.execute(VisualConceptDto);
-
-      
-
       res.status(201).json({
         success: true,
         data: VisualConcept
       });
-
     } catch (error: any) {
       res.status(400).json({
         success: false,
         error: error.message
       });
     }
-      }
+  }
 
-    async getVisualConcept(req: Request, res: Response) 
-      {
-        try {
-              const { id } = req.params;
-              const VisualConcept = await this.getVisualConceptUseCase.excute(id!);
-               
-               
-
-              res.json({
-                success: true,
-                data: VisualConcept
-          });
-
+  async getVisualConcept(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const VisualConcept = await this.getVisualConceptUseCase.excute(id!);
+      res.json({
+        success: true,
+        data: VisualConcept
+      });
     } catch (error: any) {
       res.status(404).json({
         success: false,
@@ -63,18 +189,14 @@ constructor(@inject(Types.CreateVisualConceptUseCase)  private createVisualConce
     }
   }
 
-    async deleteVisualConcept(req: Request, res: Response) {
+  async deleteVisualConcept(req: Request, res: Response) {
     try {
-      
-        const { id } = req.params;
-        const VisualConcept = await this.deleteVisualConceptUseCase.execute(id!)
-      
-
+      const { id } = req.params;
+      const VisualConcept = await this.deleteVisualConceptUseCase.execute(id!);
       res.json({
         success: true,
         data: VisualConcept
       });
-
     } catch (error: any) {
       res.status(500).json({
         success: false,
@@ -83,19 +205,16 @@ constructor(@inject(Types.CreateVisualConceptUseCase)  private createVisualConce
     }
   }
 
-       async updateVisualConcept(req: Request, res: Response) 
-      {
-        try {
-              const { id } = req.params;
-              const VisualConcept = await this.updateVisualConceptUseCase.execute(id!,req.body)
-               
-               
-
-              res.json({
-                success: true,
-                data: VisualConcept
-          });
-
+  async updateVisualConcept(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const imagePath = req.file ? req.file.filename : null;
+      const updateData = imagePath ? { ...req.body, picture: imagePath } : req.body;
+      const VisualConcept = await this.updateVisualConceptUseCase.execute(id!, updateData);
+      res.json({
+        success: true,
+        data: VisualConcept
+      });
     } catch (error: any) {
       res.status(404).json({
         success: false,
@@ -104,17 +223,13 @@ constructor(@inject(Types.CreateVisualConceptUseCase)  private createVisualConce
     }
   }
 
-      async listVisualConcept(req: Request, res: Response) {
+  async listVisualConcept(req: Request, res: Response) {
     try {
-      
-      const VisualConcept = await this.listVisualConceptUseCase.execute()
-      
-
+      const VisualConcept = await this.listVisualConceptUseCase.execute();
       res.json({
         success: true,
         data: VisualConcept
       });
-
     } catch (error: any) {
       res.status(500).json({
         success: false,
