7535b09e7a6958cb5958f4cc201e1a6787b01e0e
Agustin A. Carbajal Romero <agustin030902@gmail.com>
2025-12-14 23:44:53 -0500

    add realName to artist

--- backend/src/application/dtos/artist/ArtistResponseDto.ts
+++ backend/src/application/dtos/artist/ArtistResponseDto.ts
@@ -11,6 +11,8 @@ export class ArtistResponseDto {
     public readonly DebutDate: string,
     public readonly Status: string,
     public readonly groupHistory?:GroupResponseDTO[],
+    public readonly realName?: string,
+
     // public readonly groupHistory?: Array<{
     //   groupId: number;
     //   role: string;
@@ -27,7 +29,6 @@ export class ArtistResponseDto {
       activityId: number;
       accepted: boolean;
     }>
-    
   ) {}
 
   static fromEntity(artist: Artist): ArtistResponseDto {
@@ -38,27 +39,37 @@ export class ArtistResponseDto {
       artist.ArtistName,
       artist.DebutDate.toDateString(),
       artist.Status.toString(),
-      
-       
+      undefined,
+      artist.realName
       
     );
   }
 
-  static toEntity(artist:any):Artist {
+  static toEntity(artist: any): Artist {
+    return new Artist({
+      ApprenticeId: artist.idAp,
+      GroupId: artist.idGr,
+      ArtistName: artist.nombreArtistico,
+      DebutDate: artist.fechaDebut,
+      Status: artist.estadoArtista,
+      realName: artist.aprendiz?.nombreCompleto,
+    });
+  }
+  // static toEntity(artist:any):Artist {
       
     
-    return new Artist({ 
-      ApprenticeId:artist.idAp,
-      GroupId:artist.idGr,
-      ArtistName:artist.nombreArtistico,
-      DebutDate: artist.fechaDebut,
-      Status:artist.estadoArtista,
-     
+  //   return new Artist({ 
+  //     ApprenticeId:artist.idAp,
+  //     GroupId:artist.idGr,
+  //     ArtistName:artist.nombreArtistico,
+  //     DebutDate: artist.fechaDebut,
+  //     Status:artist.estadoArtista,
+  //     realName: artist.nombreCompleto
  
-      }
+  //     }
      
-    )
-  }
+  //   )
+  // }
 
    static toEntityForManager(artist:any):Artist {
   
