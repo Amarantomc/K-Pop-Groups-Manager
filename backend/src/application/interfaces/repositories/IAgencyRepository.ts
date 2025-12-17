1fcd2dc0a9c500ed3a63f1e3a88738d9d1369eb7
Agustin A. Carbajal Romero <agustin030902@gmail.com>
2025-12-17 03:30:54 -0500

    repository implentations

--- backend/src/application/interfaces/repositories/IAgencyRepository.ts
+++ backend/src/application/interfaces/repositories/IAgencyRepository.ts
@@ -1,3 +1,4 @@
+import type { Activity, Album, Apprentice, Concept, Contract, Income, Song } from "../../../domain";
 import { Agency } from "../../../domain/entities/Agency";
 import type { Artist } from "../../../domain/entities/Artist";
 import type { Group } from "../../../domain/entities/Group";
@@ -12,5 +13,14 @@ export interface IAgencyRepository
 	findAll(): Promise<Agency[]>;
 	artistsWithActiveContracts(AgencyId:number,date: Date | string):Promise<Artist[]>;
 	groupsWithActiveContracts(AgencyId:number,date: Date | string):Promise<Group[]>;
+	groupByAgency(AgencyId:number):Promise<Group[]>;
+	apprenticeByAgency(AgencyId:number):Promise<Apprentice[]>;
+	artistByAgency(AgencyId:number):Promise<Artist[]>;
+	conceptByAgency(AgencyId:number):Promise<Concept[]>;
+	songByAgency(AgencyId:number):Promise<Song[]>;
+	albumByAgency(AgencyId:number):Promise<Album[]>;
+	activityByAgency(AgencyId:number):Promise<Activity[]>;
+	contractByAgency(AgencyId:number):Promise<Contract[]>;
+	incomeByAgency(AgencyId:number): Promise<Income>;
 
 }
