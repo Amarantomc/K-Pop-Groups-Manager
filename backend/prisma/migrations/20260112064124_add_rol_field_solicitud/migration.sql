ab5536e4737393c2dadd6bbb77a7fc33a2eed900
Ronald_47 <132956449+Ronald1301@users.noreply.github.com>
2026-01-12 01:53:46 -0500

    create migrate role  solicitud

---
+++ backend/prisma/migrations/20260112064124_add_rol_field_solicitud/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "public"."AprendizSolicitaGrupo" ALTER COLUMN "rol" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "public"."ArtistaSolicitaGrupo" ALTER COLUMN "rol" DROP DEFAULT;
